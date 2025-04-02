e95834f344b3e5ce11c3fbc1e62012c4a206e5d7
k1nderko <107281239+k1nderko@users.noreply.github.com>
2025-04-02 16:15:08 +0300

    fix folders

--- main.py
+++ main.py
@@ -49,9 +49,9 @@ class HttpHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         pr_url = urllib.parse.urlparse(self.path)
         if pr_url.path == '/':
-            self.send_html_file('index.html')
+            self.send_html_file('templates/index.html')
         elif pr_url.path == '/message':
-            self.send_html_file('message.html')
+            self.send_html_file('templates/message.html')
         elif pr_url.path == '/read':
             try:
                 with open('storage/data.json', 'r') as file:
@@ -64,7 +64,7 @@ class HttpHandler(BaseHTTPRequestHandler):
             if pathlib.Path().joinpath(pr_url.path[1:]).exists():
                 self.send_static()
             else:
-                self.send_html_file('error.html', 404)
+                self.send_html_file('templates/error.html', 404)
 
     def send_html_file(self, filename, status=200):
         self.send_response(status)
